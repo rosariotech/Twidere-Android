17a5ae4c2b040e8c7be13db6da522c8ab155113f
Richard Rosario <richard.rosario@autoglass.com.br>
2019-02-14 13:47:48 -0200

    Mais arquivos

--- twidere/src/androidTest/kotlin/org/mariotaku/ktextension/ColorExtensionsTest.kt
+++ twidere/src/androidTest/kotlin/org/mariotaku/ktextension/ColorExtensionsTest.kt
@@ -4,15 +4,11 @@ import android.graphics.Color
 import org.junit.Assert
 import org.junit.Test
 
-/**
- * Created by mariotaku on 2017/1/8.
- */
-class ColorExtensionsTest {
+class ColorExtensionsText{
     @Test
-    fun testToHexColor() {
+    fun testToHexColor(){
         Assert.assertEquals("#FFFF0000", toHexColor(Color.RED, format = HexColorFormat.ARGB))
         Assert.assertEquals("#FF0000", toHexColor(Color.RED, format = HexColorFormat.RGB))
         Assert.assertEquals(Color.RED, Color.parseColor(toHexColor(Color.RED)))
     }
-
 }
