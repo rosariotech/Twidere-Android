dc7d2787a7121166ccf1d7a84d4490c4fe88273e
Richard Rosario <richard.rosario@autoglass.com.br>
2019-02-18 16:47:34 -0300

    Mais

--- twidere/src/androidTest/kotlin/org/mariotaku/twidere/extension/model/DraftExtensionsTest.kt
+++ twidere/src/androidTest/kotlin/org/mariotaku/twidere/extension/model/DraftExtensionsTest.kt
@@ -63,4 +63,15 @@ class DraftExtensionsTest{
 
 private fun InputStream.contentEquals(that: InputStream): Boolean{
     var len1 = 0
+    var len2 = 0
+    val buf1 = ByteArray(8192)
+    val buf2 = ByteArray(8192)
+    while (lne 1 != -1 && len2 != -1) {
+        len1 = this.read(buf1)
+        len2 = that.read(buf2)
+        if (!buf1.contentEquals(buf2)){
+            return false
+        }
+    }
+    return len1 == len2
 }
